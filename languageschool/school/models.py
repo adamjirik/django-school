b22e92aaa23fa87abaa0d8bd9a2a628a4d41be7d
Adam Jirik <adam.jirik1@gmail.com>
2018-07-18 22:07:31 +0200

    models created, migrations made, tests written to check relationships

--- languageschool/school/models.py
+++ languageschool/school/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# class Person(models.Model):
+#
+#     first_name = models.TextField(max_length=50)
+#     last_name = models.TextField(max_length=50)
+#
+#     class Meta:
+#         abstract = True
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Classroom(models.Model):
+    room_name = models.TextField(max_length=15)
+    seats = models.IntegerField()
+
+class Group(models.Model):
+    group_name = models.TextField(max_length=10)
+    students = models.ManyToManyField(Student)
+    language = models.TextField()
+    teacher = models.OneToOneField(Teacher, on_delete=models.CASCADE)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+
+
+class Assignment(models.Model):
+    description = models.TextField(max_length=80)
+    due_date = models.DateField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student, through='StudentAssignment')
+
+
+class StudentAssignment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    grade = models.FloatField()
--- languageschool/school/tests.py
+++ languageschool/school/tests.py
@@ -1,3 +1,65 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import TestCase
+from .models import Student, Group, Classroom, Teacher, Assignment, StudentAssignment
+from django.contrib.auth.models import User
+import datetime
 
 # Create your tests here.
+class ModelTest(TestCase):
+
+    def test_can_make_a_group_with_teacher_student_classroom(self):
+        user1 = User.objects.create(username='test', password='123')
+        user2 = User.objects.create(username='test2', password='123')
+        user1.save()
+        user2.save()
+
+        st = Student.objects.create(user = user1)
+        te = Teacher.objects.create(user = user2)
+        st.save()
+        te.save()
+
+        classroom = Classroom.objects.create(room_name = 'test class', seats = 4)
+
+        classroom.save()
+
+        group = Group.objects.create(language = 'spanish', group_name = 'group test', classroom = classroom, teacher = te)
+        group.save()
+        group.students.add(st)
+
+class AssignmentTest(TestCase):
+    def test_assignment_can_give_grades_to_different_students(self):
+        user1 = User.objects.create(username='test', password='123')
+        user2 = User.objects.create(username='test2', password='123')
+        user3 = User.objects.create(username='test3', password='123')
+        st = Student.objects.create(user=user1)
+        st2 = Student.objects.create(user=user2)
+        te = Teacher.objects.create(user=user3)
+        st.save()
+        st2.save()
+        te.save()
+
+        classroom = Classroom.objects.create(room_name='test class', seats=4)
+        classroom.save()
+
+        group = Group.objects.create(language='spanish', group_name='group test', classroom=classroom, teacher=te)
+        group.save()
+        group.students.set([st, st2])
+
+        assign = Assignment.objects.create(description='test',due_date=datetime.datetime.now(), group=group)
+        assign2 = Assignment.objects.create(description='test2',due_date=datetime.datetime.now(), group=group)
+        grade = StudentAssignment(student = st, assignment=assign, grade=99.9)
+        grade2 = StudentAssignment(student = st2, assignment=assign, grade=88.8)
+        grade3 = StudentAssignment(student= st, assignment = assign2, grade = 80.0)
+        grade.save()
+        grade2.save()
+        grade3.save()
+        assign_students = assign.students.all()
+        self.assertIn(st, assign_students)
+        self.assertIn(st2, assign_students)
+        st_assignments = StudentAssignment.objects.filter(student=st)
+        st_grades = []
+        for assignment in st_assignments:
+            st_grades.append(assignment.grade)
+        self.assertIn(80.0, st_grades)
+
+
